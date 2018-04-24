bb8adfa795cb0deb0b1e4afb71ce1ffc3f93a49f
Moneem Saadaoui <contact.moneem@gmail.com>
2018-04-24 11:41:41 +0100

    volley works !!

--- app/src/main/java/rrdl/be4care/Controllers/Activities/MainActivity.java
+++ app/src/main/java/rrdl/be4care/Controllers/Activities/MainActivity.java
@@ -2,31 +2,40 @@ package rrdl.be4care.Controllers.Activities;
 
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 import com.luseen.spacenavigation.SpaceItem;
 import com.luseen.spacenavigation.SpaceNavigationView;
 import com.luseen.spacenavigation.SpaceOnClickListener;
 
-import rrdl.be4care.Controllers.Fragments.DocumentsFragment;
-import rrdl.be4care.Controllers.Fragments.ProfileFragment;
-import rrdl.be4care.Controllers.Fragments.SearchFragment;
-import rrdl.be4care.Controllers.Fragments.ShortcutFragment;
+import rrdl.be4care.Controllers.Fragments.MainUIFragments.DocumentsFragment;
+import rrdl.be4care.Controllers.Fragments.MainUIFragments.ProfileFragment;
+import rrdl.be4care.Controllers.Fragments.MainUIFragments.SearchFragment;
+import rrdl.be4care.Controllers.Fragments.MainUIFragments.ShortcutFragment;
 import rrdl.be4care.R;
 
 public class MainActivity extends AppCompatActivity implements ProfileFragment.OnFragmentInteractionListener,
         SearchFragment.OnFragmentInteractionListener, ShortcutFragment.OnFragmentInteractionListener, DocumentsFragment.OnFragmentInteractionListener {
 
     SpaceNavigationView spaceNavigationView;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         /*Intent intent=new Intent(MainActivity.this,Main2Activity.class);
         startActivity(intent);*/
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Window window = getWindow();
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(Color.TRANSPARENT);}
         final android.support.v4.app.FragmentManager fm = getSupportFragmentManager();
         SpaceNavigationView spaceNavigationView = (SpaceNavigationView) findViewById(R.id.space);
         spaceNavigationView.initWithSaveInstanceState(savedInstanceState);
@@ -39,7 +48,7 @@ public class MainActivity extends AppCompatActivity implements ProfileFragment.O
         spaceNavigationView.setSpaceOnClickListener(new SpaceOnClickListener() {
             @Override
             public void onCentreButtonClick() {
-                Intent intent = new Intent(MainActivity.this, WelcomeActivity.class);
+                Intent intent = new Intent(MainActivity.this, AddDocumentActivity.class);
                 startActivity(intent);
             }
 
@@ -69,6 +78,21 @@ public class MainActivity extends AppCompatActivity implements ProfileFragment.O
             }
         });
     }
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            View decorView=getWindow().getDecorView();
+            decorView.setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+        }
+    }
+
 
     @Override
     public void onFragmentInteraction(Uri uri) {
