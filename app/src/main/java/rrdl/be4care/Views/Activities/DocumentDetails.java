92c422da00b7455d5af613f516010c65ebca3dce
Moneem Saadaoui <contact.moneem@gmail.com>
2018-05-09 11:37:55 +0100

    Page links fixed --app now exits on backpress

---
+++ app/src/main/java/rrdl/be4care/Views/Activities/DocumentDetails.java
@@ -0,0 +1,15 @@
+package rrdl.be4care.Views.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import rrdl.be4care.R;
+
+public class DocumentDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_document_details);
+    }
+}
